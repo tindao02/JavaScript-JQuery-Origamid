6d89b7f1b0e9c4415c88cc7062aa10484189dc0b
timdao02 <timdao02@hotmail.com>
2022-11-06 13:02:18 -0300

    finalizando aula 2-5 condicional

---
+++ 05-condicionais/app.js
@@ -0,0 +1,20 @@
+console.log('CONDICIONAIS');
+
+var idade = 26;
+
+if(idade < 20) 
+{
+    console.log('Idade menor que 20');
+}
+else if (idade === 25)
+{
+    console.log('Idade igual a 25');
+}
+else if((idade >= 20) && (idade < 25))
+{
+    console.log('Idade maior que 20 e menor que 25');
+}
+else
+{
+    console.log('Outra idade');
+}
