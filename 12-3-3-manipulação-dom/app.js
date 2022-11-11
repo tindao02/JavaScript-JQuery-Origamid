54a619e72b14ae72f50d88b77d2f7dfb519226ce
timdao02 <timdao02@hotmail.com>
2022-11-11 07:04:48 -0300

    finalizando aula 3-4 maniupulação dom

--- 12-3-3-manipulação-dom/app.js
+++ 12-3-3-manipulação-dom/app.js
@@ -3,5 +3,19 @@ console.log('Manipulação DOM');
 // Adiciona Texto no fim
 $('p').append('(append adiciona no fim)')
 
-// Adicionar valor do método no fim
+// remover de uma variável e adicionar no fim de outras
 $('.teste1').append($('.teste2'))
+
+// .html - pega elemento do primeiro e transforma em string
+var primeiroSpan = $('p').html();
+console.log('.html: ', primeiroSpan);
+
+// Mudar o valor todos os elementos selecionados
+// $('p').html('HTML teste')
+
+// .text('<p>Conteúdo</p>') - adiciona p conteúdo, porém não renderiza o HTML dentro dele
+// $('p').text('teste text')
+
+// .text - pego todos os valores do elemento selecionado
+var text = $('p').text();
+console.log(text);
