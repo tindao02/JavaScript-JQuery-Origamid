4b1045845889545dc22093879e0a09a7f336358d
timdao02 <timdao02@hotmail.com>
2022-11-20 23:46:10 -0300

    3-15 traversing 2

---
+++ 23-3-14-traversing-2/app.js
@@ -0,0 +1,19 @@
+// console.log('3-14 Traversing 2');
+
+// // .first() - selectiona apenas o primeiro elemento
+// $('.teste01').first().css('background', '#12345648')
+
+// // .last() - selectiona apenas o ultimo elemento
+// $('.teste01').last().css('background', '#F2345648')
+
+// // .prev() - selectiona o elemento anterior
+// // $(element).prev();
+
+// // .next() - próximo elemento
+// // $(element).next()
+
+// // .eq() - selectiona o elemento para index dele
+// $('.teste01').eq(1).css('color', 'red');
+
+// .not() remove da seleção
+$('div').not('.div-teste-01').css('background', '#45678955')
